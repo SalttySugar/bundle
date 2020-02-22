879703498ad51c285de80f6c4b4b2a6509d06885
Bogdan <AlenWhalker@gmail.com>
2020-02-22 14:52:14 +0300

    fixes

---
+++ main2.js
@@ -0,0 +1,41 @@
+
+function remove_from_cart()
+{
+    let container = $(this).parents('.button-group');
+    container.empty();
+
+    let buy_button = $('<button class="buy-button product-item-button">Добавить в корзину</button>');
+    buy_button.click(add_to_cart);
+
+    container.append(buy_button);
+}
+
+
+function add_to_cart()
+{
+    let container = $(this).parent(".button-group");
+    $(this).unbind();
+    $(container).empty();
+
+    let cart_controls = $('<div class="product-item-cart-control"> </div>');
+
+    let cart_trash = $('<i class="icon icon-trash"></i>');
+    let cart_quantity = $('<span class="quantity">1шт</span>');
+    let cart_add = $('<i class="icon icon-add"></i>');
+
+    cart_trash.click(remove_from_cart);
+    cart_add.click(function(){
+        let current = Number.parseInt(cart_quantity.text());
+        cart_quantity.text(++current + "шт");
+    });
+
+    cart_controls.append(cart_trash , cart_quantity , cart_add);
+
+    let order_button = $(' <button class="button product-item-button order-button"> Оформить </button>');
+
+    $(container).append(cart_controls , order_button);
+}
+
+
+
+$(".button-group button.buy-button").click(add_to_cart);
